8f452be23a5e9a2d43382dce981d10753c7ca123
erickexinlu <erickexinlu@gmail.com>
2022-12-25 15:47:34 -0800

    SCH-13 feat: basic skeleton with flexbox

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css'
+
+const LandingPage = () => {
+  return (
+    <div className='wrapper'>
+        <div className='header'>header</div>
+        <div className='title'>
+            <span>Schedulii</span>            
+            <div className='loginButton'>loginButton</div>
+        </div>
+        
+        <div className='footer'>footer</div>
+    </div>
+  )
+}
+
+export default LandingPage

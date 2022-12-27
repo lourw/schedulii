2814784cf8252c641b1c83cbdd9dcd73838487bd
Britney Ng <britneyng1@gmail.com>
2022-12-26 18:31:53 -0800

    SCH-22 feat: added simple navbar component for react routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { LandingPage } from './pages';
+import { LandingPage, Homepage } from './pages';
+import { Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/home" element={<Homepage/>}/>
+      </Routes>
     </div>
   );
 }

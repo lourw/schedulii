efef644eeb6d470d9732e5bdbaf7b7f3ad2a1384
Britney Ng <britneyng1@gmail.com>
2022-12-28 23:45:54 -0800

    SCH-14 created eventcard component

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import '../pages/Homepage.css';
+import React from 'react'
+import Navbar from '../components/Navbar'
+import EventCard from '../components/EventCard'
+import '../pages/Homepage.css'
 
 const Homepage = () => {
   return (
@@ -9,9 +10,11 @@ const Homepage = () => {
       <section className="container">
         <div id="left">
           <h1>Your Calendar</h1>
+          <p>The calendar component can go here.</p>
         </div>
         <div id="right">
           <h1>Your Events</h1>
+          <EventCard title="Sample Event" description="Event description here." />
         </div>
       </section>
     </>

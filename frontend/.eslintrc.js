53452e622aaa5c0d523ed693ea0afe136ea9078a
erickexinlu <erickexinlu@gmail.com>
2022-12-29 15:30:42 -0800

    Render dateLabel and timeText alongside grid

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    extends: '@react-native-community',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': ['error'],
+    },
+    overrides: [
+      {
+        files: ['*.ts', '*.tsx'],
+        rules: {
+          'no-undef': 'off',
+        },
+      },
+    ],
+  };

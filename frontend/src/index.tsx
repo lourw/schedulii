67fb40a2a1a19235f66adba961c1b688fe1b9a7e
erickexinlu <erickexinlu@gmail.com>
2022-12-29 19:01:43 -0800

    Resolve merge conflicts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Homepage from './pages/Homepage';
+import CreateEvent from './pages/CreateEvent';
+import Calendar from './components/Calendar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <App />
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/home" element={<Homepage/>}/>
+        <Route path="/create-event" element={<CreateEvent/>}/>
+        <Route path="/availability" element={<Calendar 
+                                                numDays={3}
+                                                startTime={9}
+                                                endTime={15} />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 

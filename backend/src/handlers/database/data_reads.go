afe394f2e7dac2c4b3166daba78971f0aa65ac2c
Britney Ng <britneyng1@gmail.com>
2023-01-04 18:47:46 -0800

    SCH-57 changed CreateUser to helper function and make placeholder route

---
+++ backend/src/handlers/database/data_reads.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	models "schedulii/src/models"
+	"github.com/gin-gonic/gin"
+)
+
+
+func ReadUser(env *models.Env) gin.HandlerFunc {
+
+	fn := func(c *gin.Context) {
+	}
+	return gin.HandlerFunc(fn)
+}

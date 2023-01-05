e38ce2d075b190d0d00835777d0f232ef02a6fed
Louie Weng <louieweng@outlook.com>
2023-01-05 15:30:31 -0800

    SCH-56 added tests for protected routes

---
+++ frontend/src/__test__/components/ProtectedRoute.test.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ProtectedRoute from "../../components/ProtectedRoute";
+import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import type AuthContextDataType from "../../context/AuthContextDataType";
+
+describe("Protected Route", () => {
+    let validContext: AuthContextDataType;
+
+    beforeEach(() => {
+        validContext = {
+            token: "invalid-token",
+            onLogin: (): void => { },
+            onLogout: (): void => { },
+        };
+    });
+
+    it("should render default route when no user is validated", async () => {
+        validContext.token = null;
+
+        render(
+            <MemoryRouter initialEntries={["/protected"]}>
+                <Routes>
+                    <Route path="/" element={
+                        <p>Home</p>
+                    } />
+                    <Route path="/protected" element={
+                        <AuthContext.Provider value={validContext}>
+                            <ProtectedRoute><p>Child</p></ProtectedRoute>
+                        </AuthContext.Provider>
+                    } />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText("Home")).toBeInTheDocument();
+    });
+
+    it("should render children if user is validated", () => {
+        // localStorage.setItem("token", JSON.stringify("test-token"));
+        render(
+            <MemoryRouter initialEntries={["/protected"]}>
+                <Routes>
+                    <Route path="/" element={
+                        <p>Home</p>
+                    } />
+                    <Route path="/protected" element={
+                        <AuthContext.Provider value={validContext}>
+                            <ProtectedRoute><p>Child</p></ProtectedRoute>
+                        </AuthContext.Provider>
+                    } />
+                </Routes>
+            </MemoryRouter>
+        );
+        expect(screen.getByText("Child")).toBeInTheDocument();
+    });
+});
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,18 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./context/AuthProvider";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <AuthProvider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </AuthProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
 

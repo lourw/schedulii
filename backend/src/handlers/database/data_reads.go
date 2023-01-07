ce89ced302adf9b10dee00bd61875922da2d315a
Britney Ng <britneyng1@gmail.com>
2023-01-06 16:07:22 -0800

    SCH-43 added error check for ShouldBindQuery

--- backend/src/handlers/database/data_reads.go
+++ backend/src/handlers/database/data_reads.go
@@ -12,15 +12,19 @@ import (
 func ReadUser(env *models.Env) gin.HandlerFunc {
 
 	fn := func(c *gin.Context) {
-
 		var user models.User
+		err := c.ShouldBindQuery(&user)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-		c.ShouldBindQuery(&user)
-		err := env.DB.QueryRow(context.Background(), data.SelectUser(), user.Username).Scan(&user.Username)
-			if err != nil {
+		readError := env.DB.QueryRow(context.Background(), data.SelectUser(), user.Username).Scan(&user.Username)
+			if readError != nil {
 				c.JSON(http.StatusBadRequest, "Could not find user.")
 				return
 			}
+
 		c.JSON(http.StatusOK, user)
 	}
 	return gin.HandlerFunc(fn)

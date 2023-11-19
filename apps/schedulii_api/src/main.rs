de2970b8b62721a949dbc66092b1d71c0817a79a
Louie Weng <louieweng@outlook.com>
2023-11-19 18:36:11 -0500

    feat: add backend rust api server

--- apps/schedulii_api/src/main.rs
+++ apps/schedulii_api/src/main.rs
@@ -1,4 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, Server};
+use axum_prometheus::PrometheusMetricLayer;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+  let (prometheus_layer, metric_handler) = PrometheusMetricLayer::pair();
+
+  let app = Router::new()
+    .route("/", get(|| async { "Hello, World!" }))
+    .route("/metrics", get( || async move { metric_handler.render() }))
+    .layer(prometheus_layer);
+
+  let addr = SocketAddr::from(([0, 0, 0, 0], 9000));
+  Server::bind(&addr)
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
 
+#[cfg(test)]
+mod tests {
+  use axum::body::Body;
+  use axum::http::{Request, StatusCode};
+  use axum::{routing::get, Router};
+  use axum_prometheus::PrometheusMetricLayer;
+  use tower::ServiceExt;
+
+  #[tokio::test]
+  async fn main() {
+    let (prometheus_layer, metric_handler) = PrometheusMetricLayer::pair();
+
+    let app = Router::new()
+      .route("/", get(|| async { "Hello World!" }))
+      .route("/metrics", get( || async move { metric_handler.render() }))
+      .layer(prometheus_layer);
+
+    let response = app
+      .oneshot(Request::builder()
+               .uri("/")
+               .body(Body::empty())
+               .unwrap())
+      .await
+      .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+  }
+}
---
+++ docs/nx.md
@@ -0,0 +1,5 @@
+## Setup of repo
+
+1. `nx create-nx-workspace schedulii --preset=react-monorepo`
+2. `pnpm install @monodon/rust`
+3. `nx generate @monodon/rust:binary schedulii-api`

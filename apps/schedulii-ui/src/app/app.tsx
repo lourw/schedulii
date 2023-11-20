72f57015abaf56a78cec034c5348942202eda985
Louie Weng <louieweng@outlook.com>
2023-11-19 19:57:49 -0500

    feat: add frontend shell

--- apps/schedulii-ui/src/app/app.tsx
+++ apps/schedulii-ui/src/app/app.tsx
@@ -1,13 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import Landing from '../pages/Landing';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="schedulii-ui" />
-    </div>
+    <>
+      <div className="m-16">
+        <h1 className="text-4xl mb-8">Schedulii</h1>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- apps/schedulii-ui/src/main.tsx
+++ apps/schedulii-ui/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );

cdeba6940d09354e15a205dc51042589da65dab7
Louie Weng <louieweng@outlook.com>
2023-11-23 00:20:45 -0500

    add api dependencies for sqlx

--- apps/schedulii_api/src/main.rs
+++ apps/schedulii_api/src/main.rs
@@ -1,15 +1,34 @@
 use axum::{routing::get, Router, Server};
 use axum_prometheus::PrometheusMetricLayer;
 use std::net::SocketAddr;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+#[derive(Clone)]
+struct AppState {
+    _db_pool: sqlx::PgPool,
+}
 
 #[tokio::main]
 async fn main() {
+    let database_url = match env::var("DATABASE_URL") {
+        Ok(val) => val,
+        Err(e) => e.to_string(),
+    };
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .unwrap();
+
     let (prometheus_layer, metric_handler) = PrometheusMetricLayer::pair();
+    let state = AppState { _db_pool: pool };
 
     let app = Router::new()
         .route("/", get(|| async { "Hello, World" }))
         .route("/metrics", get(|| async move { metric_handler.render() }))
-        .layer(prometheus_layer);
+        .layer(prometheus_layer)
+        .with_state(state);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 9000));
     Server::bind(&addr)

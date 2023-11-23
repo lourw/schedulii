e7acfdb20de82db51afa5b60e8ed6f3c85b5efc5
Louie Weng <louieweng@outlook.com>
2023-11-22 22:30:45 -0500

    chore: fix linting issues (#2)
    fix linting issues

--- apps/schedulii-ui/src/app/app.spec.tsx
+++ apps/schedulii-ui/src/app/app.spec.tsx
@@ -1,15 +1,15 @@
 import { render } from '@testing-library/react';
 
 import App from './app';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
     expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome schedulii-ui/gi)).toBeTruthy();
   });
 });

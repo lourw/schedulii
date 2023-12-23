ebaa2b62d03ac13cce206cba7717ea4fa54486a0
Britney Ng <britneyng1@gmail.com>
2023-12-23 15:49:25 -0800

    remove unused struct

--- apps/schedulii_api/src/models/event.rs
+++ apps/schedulii_api/src/models/event.rs
@@ -10,11 +10,3 @@ pub struct Event {
     pub end_time: DateTime<Utc>,
     pub location: String,
 }
-
-#[derive(Debug, FromRow, Serialize, Deserialize)]
-pub struct NewEvent {
-    pub event_name: String,
-    pub start_time: DateTime<Utc>,
-    pub end_time: DateTime<Utc>,
-    pub location: String,
-}

b00db17c8296895bf9771fe6d5c9649ce278a699
Britney Ng <britneyng1@gmail.com>
2023-12-23 16:19:48 -0800

    fix formatting

--- apps/schedulii-ui/src/pages/Home.tsx
+++ apps/schedulii-ui/src/pages/Home.tsx
@@ -1,14 +1,11 @@
-interface HomeProps {
-
-}
+interface HomeProps {}
 
 const Home: React.FC<HomeProps> = (props) => {
   return (
     <div>
       <div>Home</div>
-      
     </div>
   );
-}
+};
 
 export default Home;
